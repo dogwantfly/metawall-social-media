77f9edb400bc1886b328f6581cade38d0f3a4fda
hsin yu <tina2793778@gmail.com>
2024-05-12 21:01:59 +0800

    refactor: 💡 調整 api base url

--- src/api/base.js
+++ src/api/base.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default axiosInstance;
