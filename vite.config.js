2e228b71b6cb882fdbb546a886503c00d747e0de
hsin yu <tina2793778@gmail.com>
2024-05-13 14:53:03 +0800

    refactor: 💡 調整 vite 設定

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
+export default defineConfig({
     plugins: [react()],
-    define: {
-      __APP_ENV__: JSON.stringify(env.VITE_API_BASE_URL),
-    },
     base:　process.env.NODE_ENV === 'production' ? '/metawall-social-media/' : '/',
-  };
-});
+  });
