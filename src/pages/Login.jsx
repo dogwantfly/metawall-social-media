7ab197b10a0933952bd8223cbfb705fe3b0c24e5
hsin yu <tina2793778@gmail.com>
2024-05-21 22:34:40 +0800

    feat: 🎸 新增登入畫面

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,41 @@
+import { Link } from 'react-router-dom';
+
 function Login() {
   return (
     <>
-      <div>
-        <h1>Login</h1>
-        <img
-          src='https://firebasestorage.googleapis.com/v0/b/metawall-social-media.appspot.com/o/images%2Fmetawall-1.webp?alt=media&token=d2dd5565-50d6-4417-b299-743453adbb6c'
-          alt='Login'
-        />
-      </div>
+      <form>
+        <h2 className='text-2xl font-bold text-center mb-9'>
+          到元宇宙展開全新社交圈
+        </h2>
+        <div className='relative mb-4'>
+          <input
+            type='email'
+            className='peer block min-h-[auto] w-full border-2 border-black-custom bg-white px-6 py-4 leading-[1.6] outline-none transition-all duration-200 ease-linear font-azeret-mono focus:placeholder:opacity-100 peer-focus:text-black-custom data-[twe-input-state-active]:placeholder:opacity-100 placeholder:font-azeret-mono'
+            id='email'
+            placeholder='Email'
+          />
+        </div>
+
+        <div className='relative mb-8'>
+          <input
+            type='password'
+            className='peer block min-h-[auto] w-full border-2 border-black-custom bg-white px-6 py-4 leading-[1.6] outline-none transition-all duration-200 ease-linear font-azeret-mono focus:placeholder:opacity-100 peer-focus:text-black-custom data-[twe-input-state-active]:placeholder:opacity-100 placeholder:font-azeret-mono'
+            id='password'
+            placeholder='Password'
+          />
+        </div>
+        <button
+          type='button'
+          className='w-full rounded bg-blue-custom border-2 border-black-custom button-shadow py-4 font-azeret-mono font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-blue-custom/80  focus:outline-none focus:ring-0 active:bg-blue-custom/80 active:shadow-blue-custom-2 mb-4'
+        >
+          登入
+        </button>
+        <div className='flex justify-center'>
+          <Link to='/auth/signUp' className='hover:text-blue-custom p-3'>
+            註冊帳號
+          </Link>
+        </div>
+      </form>
     </>
   );
 }
