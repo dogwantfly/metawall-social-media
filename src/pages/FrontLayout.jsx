49a26c421a95b9cfab5e9855c89a0a2ed57df1cc
hsin yu <tina2793778@gmail.com>
2024-06-01 20:37:55 +0800

    refactor: 💡 調整 icon

--- src/pages/FrontLayout.jsx
+++ src/pages/FrontLayout.jsx
@@ -247,19 +247,24 @@ function FrontLayout() {
             }}
             className='rounded-full flex items-center gap-4 font-bold hover:text-blue-custom hover:ease-in-out focus:text-blue-custom active:text-blue-custom group'
           >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={2}
-              className='size-6 group-hover:stroke-white'
+            <div
+              className='border-2 border-black rounded-full bg-blue-custom-light flex items-center justify-center group-hover:bg-blue-custom'
+              style={{ height: '50px', width: '50px' }}
             >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
-              />
-            </svg>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={2}
+                className='size-6 group-hover:stroke-white'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+                />
+              </svg>
+            </div>
           </Link>
 
           <Link
